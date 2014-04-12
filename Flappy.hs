05a71d28cff4654439c2459400c052d9a4eb9c73
torii <countryroad2317@yahoo.co.jp>
2014-04-12 11:32:09 +0900

    当たり判定, スコア表示追加

--- Flappy.hs
+++ Flappy.hs
@@ -2,6 +2,7 @@
 
 import FreeGame
 import Control.Applicative
+import System.Directory
 
 winWIDTH = 480
 winHEIGHT = 520
@@ -14,6 +15,7 @@ data GameState = Opening | Playing | GameOver | Ranking deriving (Show,Eq)
 data Game = Game{
     state :: GameState,
     points :: Int,
+    highscore :: Int,
     pos :: Vec2,
     jumpPower :: Double,
     gravitiy :: Double,
@@ -24,12 +26,15 @@ data Game = Game{
     background :: Bitmap,
     bird :: Bitmap,
     upPoll :: Bitmap,
-    underPoll :: Bitmap
+    underPoll :: Bitmap,
+    scoreBoard :: Bitmap,
+    medal :: Bitmap
     }
 
-initialize font start rank back bird uppoll underpoll = return $ Game {
+initialize font start rank back bird uppoll underpoll scoreboard medal highscore = return $ Game {
     state = Opening,
     points = 0,
+    highscore = highscore,
     pos = V2 birdX 260,
     jumpPower = 0,
     gravitiy = 3,
@@ -40,7 +45,9 @@ initialize font start rank back bird uppoll underpoll = return $ Game {
     background = back,
     bird = bird,
     upPoll = uppoll,
-    underPoll = underpoll
+    underPoll = underpoll,
+    scoreBoard = scoreboard,
+    medal = medal
     }
 
 main = do
@@ -51,7 +58,10 @@ main = do
     rankBtn <- readBitmap "rank.png"
     bird <- readBitmap "bird.png"
     background <- readBitmap "background.png"
-    game <- initialize font startBtn rankBtn background bird uppoll underpoll
+    scoreboard <- readBitmap "scoreboard.png"
+    medal <- readBitmap "medal.png"
+    highscore <- read <$> readFile "score"
+    game <- initialize font startBtn rankBtn background bird uppoll underpoll scoreboard medal highscore
     runGame Windowed (BoundingBox 0 0 winWIDTH winHEIGHT) $ do
         setTitle "Flappy Bird"
         clearColor black
@@ -68,22 +78,26 @@ mainloop game = do
             tick
             mainloop next
         Playing -> do
-            translate (pos game) $ bitmap (bird game)
             drawPolls game
+            translate (pos game) $ bitmap (bird game)
             next' <- seeInput Playing game
             let y = nextPos (pos game) (jumpPower next') (gravitiy game)
             debug (font game) $ show (gravitiy game)
             showPoints (font game) $ points game
-            next <- update $ game {pos = y, jumpPower = nextJump (jumpPower next')}
+            next <- update Playing $ game {pos = y, jumpPower = nextJump (jumpPower next')}
             tick
-            mainloop $ next --game {pos = y, jumpPower = nextJump (jumpPower next')}
+            mainloop $ next
         GameOver -> do
-            --drawFrame
-            translate (V2 240 80) $ bitmap (bird game)
+            drawPolls game
+            translate (pos game) $ bitmap (bird game)
+            translate (V2 (winWIDTH/2-100) (winHEIGHT/2-80)) $ color cyan $ text (font game) 40 "Game Over"
+            drawScore game
+            next' <- update GameOver game
+            next <- seeInput GameOver next'
             tick
-            mainloop game
+            mainloop next
         
-
+--draw objects
 drawButtons game = do
     translate (V2 180 400) $ bitmap (startBtn game)
     translate (V2 300 400) $ bitmap (rankBtn game)
@@ -91,10 +105,23 @@ drawButtons game = do
 drawPolls game = mapM_ (drawPoll (upPoll game) (underPoll game)) $ map snd $ polls game
 
 drawPoll up under pos = do
-    translate (moveY pos (-220)) $ bitmap up
+    translate (moveY pos (-230)) $ bitmap up
     translate (moveY pos (220)) $ bitmap under
+
+drawScore game = do
+    translate (V2 (winWIDTH/2) (winHEIGHT/2)) $ bitmap (scoreBoard game)
+    let newScore = max (highscore game) (points game)
+    if points game > 50
+        then do
+            translate (V2 (winWIDTH/2-87) (winHEIGHT/2+5)) $ bitmap (medal game)
+        else
+            return ()
+    translate (V2 340 250)$ color white $ text (font game) 30 $ show $ points game
+    translate (V2 340 310)$ color white $ text (font game) 30 $ show newScore
     
     
+    
+--get the inputs from mouse and keyboard
 seeInput Opening game = do
     pos <- mousePosition
     t <- mouseUpL
@@ -105,25 +132,58 @@ seeInput Playing game = do
     key <- keyUp KeySpace
     return $ if mUp || key then game {jumpPower = jumpPower game - 17,gravitiy = 0} else game
     
+seeInput GameOver game = do
+    mUp <- mouseUpL
+    key <- keyUp KeySpace
+    if (key || mUp) && 460 <= posY (pos game)
+        then do
+            if highscore game < (points game)
+                then do
+                    liftIO $ removeFile "score"
+                    liftIO $ writeFile "score" $ show $ points game    
+                    return $ refresh game
+                else return game
+        else return game
+    
+
+refresh game = game{
+    state = Opening,
+    points = 0,
+    pos = V2 birdX 260,
+    jumpPower = 0,
+    gravitiy = 3,
+    polls = [(False,V2 480 200)]
+    }
+
 inStartBtn (V2 x y) = 135 <= x && x <= 225 && 376 <= y && y <= 424
 
-nextJump power = if power < 0 then power + 2 else 0
-
-
 --update functions
-update game = do
+update Playing game = do
     r <- randomness (100,350)
     let (cnt,polls') = passPoll (polls game)
     return $ game{
         points = points game + cnt, 
         polls = deletePoll.movePolls $ nextPolls r polls',
-        gravitiy = nextGravity (gravitiy game) (jumpPower game)}        
+        gravitiy = nextGravity (gravitiy game) (jumpPower game),
+        state = nextState (pos game) polls'
+        }
+
+update GameOver game = do
+    let g = nextGravity (gravitiy game) (jumpPower game)
+    return $ game{
+        gravitiy = g+1,
+        pos = nextPos (pos game) 0 (gravitiy game)
+        }
+    
 
 nextPos :: Vec2 -> Double -> Double -> Vec2
 nextPos pos pwr g
+    | 460 <= posY pos = V2 birdX 460
     | pwr < 0 = moveY pos pwr
     | 0 == pwr = moveY pos g
     | otherwise = pos
+
+nextJump power = if power < 0 then power + 2 else 0
 
 nextGravity g pwr
     | pwr == 0 && g == 0 = 1
@@ -140,11 +200,30 @@ passPoll = foldr isPassed (0,[]) where
         | posX (snd p) <= birdX = (n+1,(True,snd p):ps)
         | otherwise = (n,p:ps)
 
+nextState :: Vec2 -> [Poll] -> GameState
+nextState pos polls = if isHit pos polls then GameOver else Playing
+
+isHit :: Vec2 -> [Poll] -> Bool
+isHit pos = not.null.filter (hit pos.snd)
+
+hit :: Vec2 -> Vec2 -> Bool
+hit pos poll = 
+    (posY poll+90)-posY pos < 35 && (posX poll-70) < posX pos && posX pos < (posX poll+75)
+    || posY pos-(posY poll-87) < 35 && (posX poll-70) < posX pos && posX pos < (posX poll+75)
+    || posY pos < (posY poll-87) && abs (posX poll - posX pos) < 25
+    || (posY poll+90) < posY pos && abs (posX poll - posX pos) < 25
+    || 460 <= posY pos
+
+distance :: Vec2 -> Vec2 -> Double
+distance (V2 x1 y1) (V2 x2 y2) = sqrt $ (x2-x1)^2 + (y2-y1)^2
+
 --operate the position of objects
 posX (V2 x _) = x
 moveX (V2 x y) v = V2 (x+v) y
+posY (V2 _ y) = y
 moveY (V2 x y) v = V2 x (y+v)
 (V2 x y) .+. (V2 x' y') = V2 (x+x') (y+y')
+
 --show data with text
 debug font str = translate (V2 40 40)$ color black $ text font 25 str
 showPoints font pnt = translate (V2 (winWIDTH/2) 40)$ color white $ text font 40 $ show pnt
